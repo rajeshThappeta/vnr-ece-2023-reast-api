9f32914b3cd9a00bc42e293f90d25850bec312be
rajesh <rajesh.t7982@gmail.com>
2023-06-12 20:11:20 +0530

    user & product CRUD operations implemented

--- apis/userApi.js
+++ apis/userApi.js
@@ -1,75 +1,118 @@
 //create mini-express app
 const exp = require("express");
 const userApp = exp.Router();
-const bcryptjs=require("bcryptjs")
+const bcryptjs = require("bcryptjs");
 
 //body parser middleware
 userApp.use(exp.json());
 
-
-
 //CREATE  USER API (Routes)
 
 //route to read all users
-userApp.get("/users", (req, res) => {
- 
+userApp.get("/users", async (req, res) => {
+  //get usersCollectionObj
+  let usersCollectionObj = req.app.get("usersCollectionObj");
+  //get users
+  let users = await usersCollectionObj.find({status:true}).toArray();
+  //send res
+  res.send({ message: "all users", payload: users });
 });
 
-//route to read one user by id
-userApp.get("/users/:id", (req, res) => {
- 
+//route to read one user by username
+userApp.get("/users/:username", async (req, res) => {
+  //get usersCollectionObj
+  let usersCollectionObj = req.app.get("usersCollectionObj");
+  //get username from url
+  let usernameOfUrl = req.params.username;
+  //find user by username
+  let user = await usersCollectionObj.findOne({ username: usernameOfUrl,status:true });
+  //send res
+  res.send({ message: "one user", payload: user });
 });
 
 //route to create new user
-userApp.post("/user", async(req, res) => {
- 
+userApp.post("/user", async (req, res) => {
   //get usersCollectionObj
-  let usersCollectionObj=req.app.get('usersCollectionObj')
+  let usersCollectionObj = req.app.get("usersCollectionObj");
   //get new user from req
-  let newUser=req.body;
+  let newUser = req.body;
   //verify user's existance
-  let existingUser=await usersCollectionObj.findOne({username:newUser.username})
+  let existingUser = await usersCollectionObj.findOne({
+    username: newUser.username,
+  });
   //if user not existed
-  if(existingUser===null){
+  if (existingUser === null) {
+    //add status property to new user
+    newUser.status=true;
     //hash the password
-    let hashedPassword=await bcryptjs.hash(newUser.password,5)
+    let hashedPassword = await bcryptjs.hash(newUser.password, 5);
     //replace plain paassword with hashed password
-    newUser.password=hashedPassword;
+    newUser.password = hashedPassword;
     //create new user
-    await usersCollectionObj.insertOne(newUser)
-    res.send({message:"User creation success"})
+    await usersCollectionObj.insertOne(newUser);
+    res.send({ message: "User creation success" });
+  } else {
+    res.send({ message: "User already existed" });
   }
-  else{
-    res.send({message:'User already existed'})
-  }
+});
+
+//route to update  a user by usernme
+userApp.put("/user/:username", async (req, res) => {
+  //get usersCollectionObj
+  let usersCollectionObj = req.app.get("usersCollectionObj");
+  //get username from url
+  let usernameOfUrl = req.params.username;
+  //get modified user from client
+  let modifiedUser = req.body;
+  //update
+  await usersCollectionObj.updateOne(
+    { username: usernameOfUrl },
+    {
+      $set: {
+        ...modifiedUser,
+      },
+    }
+  );
+  //send res
+  res.send({message:"User update success"})
+});
+
+//route to delete a user by username
+userApp.delete("/user/:username", async(req, res) => {
+  //get usersCollectionObj
+  let usersCollectionObj = req.app.get("usersCollectionObj");
+  //get username from url
+  let usernameOfUrl = req.params.username;
+
+  //soft delete
+  await usersCollectionObj.updateOne({username:usernameOfUrl},{$set:{status:false}})
+  //send res
+  res.send({message:"User deleted"})
+
+});
+
+//route to delete a user by username
+userApp.get("/restore-user/:username", async(req, res) => {
+  //get usersCollectionObj
+  let usersCollectionObj = req.app.get("usersCollectionObj");
+  //get username from url
+  let usernameOfUrl = req.params.username;
+
+  //soft delete
+  await usersCollectionObj.updateOne({username:usernameOfUrl},{$set:{status:true}})
+  //send res
+  res.send({message:"User restored"})
+
 });
 
 
 
 
 
-//route to update  a user by id
-userApp.put("/user/:id", (req, res) => {
- 
-});
+userApp.post("/write-review", (req, res) => {});
 
-//route to delete a user by id
-userApp.delete("/user/:id", (req, res) => {
-
-});
-
-
-
-userApp.post('/write-review',(req,res)=>{
-
-})
-
-userApp.post('/buy-product',(req,res)=>{
-  
-})
-userApp.post('/add-to-cart',(req,res)=>{
-  
-})
+userApp.post("/buy-product", (req, res) => {});
+userApp.post("/add-to-cart", (req, res) => {});
 
 //export userApp
-module.exports=userApp;
+module.exports = userApp;
--- server.js
+++ server.js
@@ -1,34 +1,36 @@
 //CREATE EXPRESS APP
-const exp=require('express')
-const app=exp();
+const exp = require("express");
+const app = exp();
 
 //import MongoClient
-const mngClient=require('mongodb').MongoClient;
-
+const mngClient = require("mongodb").MongoClient;
 
 //connecting to MongoDB server with mongo client
-mngClient.connect('mongodb://127.0.0.1:27017/ecedb')
-.then(client=>{
+mngClient
+  .connect("mongodb://127.0.0.1:27017/ecedb")
+  .then((client) => {
     //get db obj
-    let db=client.db('ecedb')
-    //get collection obj
-    let usersCollectionObj=db.collection('users')
+    let db = client.db("ecedb");
+    //get  usercollection obj
+    let usersCollectionObj = db.collection("users");
+    //get proudct collection obj
+    let productsCollectionObj = db.collection("products");
     //share usersCollectionObj
-    app.set('usersCollectionObj',usersCollectionObj)
+    app.set("usersCollectionObj", usersCollectionObj);
+    //share usersCollectionObj
+    app.set("productsCollectionObj", productsCollectionObj);
 
-    console.log("Database connected successfully")
-})
-.catch(err=>console.log("err in db connect ",err))
+    console.log("Database connected successfully");
+  })
+  .catch((err) => console.log("err in db connect ", err));
 
-
-
-const userApp=require("./apis/userApi")
-const productApp=require('./apis/productApi')
+const userApp = require("./apis/userApi");
+const productApp = require("./apis/productApi");
 
 //if path starts with /user-api, then forward req to userApi
-app.use('/user-api',userApp)
+app.use("/user-api", userApp);
 //if path starts with /product-api, then forward req to productApi
-app.use('/product-api',productApp)
+app.use("/product-api", productApp);
 
 //assign port number to HTTP Server
-app.listen(4000,()=>console.log("server listening on port 4000..."))
+app.listen(4000, () => console.log("server listening on port 4000..."));
